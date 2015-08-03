d38901c155b774fe9e093d218cc04a2a97ce566f
Atte Kojo <atte.kojo@reaktor.fi>
2015-08-03 19:31:44 +0300

    Move git aliases to fish abbreviations

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -2,3 +2,28 @@
 
 # OPAM configuration
 . /Users/atte/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+abbr g git
+abbr ga 'git add'
+abbr gau 'git add -u'
+abbr gC 'git clean -fd'
+abbr gc 'git commit --verbose'
+abbr gca 'git commit --verbose --amend'
+abbr gco 'git checkout'
+abbr gd 'git diff'
+abbr gdc 'git diff --cached'
+abbr gf 'git fetch'
+abbr gfa 'git fetch --all'
+abbr gL 'git log --oneline --abbrev-comit --decorate=short --graph --topo-order'
+abbr gl 'git log --pretty=format:\'%C(auto)%h %C(red)%cr%C(reset) %C(cyan)%an%C(reset)%C(auto) %d %s\' --graph --topo-order'
+abbr gp 'git pull'
+abbr gP 'git push'
+abbr gPf 'git push --force'
+abbr gpr 'git pull --rebase'
+abbr gPt 'git push --tags'
+abbr gr 'git rebase'
+abbr gR 'git reset'
+abbr gra 'git rebase --abort'
+abbr grc 'git rebase --continue'
+abbr gRh 'git reset --hard'
+abbr gri 'git rebase --interactive'
+abbr gs 'git status --short'
